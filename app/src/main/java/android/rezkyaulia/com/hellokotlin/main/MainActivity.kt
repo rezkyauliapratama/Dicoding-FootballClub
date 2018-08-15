9b4a1bce4a1c4f57840c92ec1fbfeb23d4c257e9
rezky aulia pratama <rezkyauliapratama@gmail.com>
2018-08-15 18:02:16 +0700

    - add tablayout and viewpager

--- app/src/main/java/android/rezkyaulia/com/hellokotlin/main/MainActivity.kt
+++ app/src/main/java/android/rezkyaulia/com/hellokotlin/main/MainActivity.kt
@@ -7,13 +7,26 @@ import android.rezkyaulia.com.hellokotlin.R
 import android.rezkyaulia.com.hellokotlin.R.array.league
 import android.rezkyaulia.com.hellokotlin.base.BaseActivity
 import android.rezkyaulia.com.hellokotlin.data.Team
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+import android.support.v4.content.ContextCompat
+import android.view.Gravity
+import android.view.MenuItem
 import android.view.View
 import android.widget.*
+import com.app.infideap.stylishwidget.view.ATextView
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_activity_main.view.*
 import org.jetbrains.anko.*
 
 class MainActivity : BaseActivity<MainPresenter>(),MainView {
+
+    lateinit var fragments: MutableList<Fragment>
+    lateinit var fragment: Fragment
+
     override fun initPresenter(): MainPresenter {
         return MainPresenter(this)
     }
@@ -21,6 +34,7 @@ class MainActivity : BaseActivity<MainPresenter>(),MainView {
 
     private var teams: MutableList<Team> = mutableListOf()
     private lateinit var adapter: MainAdapter
+    private lateinit var tabAdapter: LfPagerAdapter
 
     private lateinit var leagueName: String
 
@@ -42,6 +56,10 @@ class MainActivity : BaseActivity<MainPresenter>(),MainView {
 
             override fun onNothingSelected(parent: AdapterView<*>) {}
         }
+
+        fragments = mutableListOf()
+        initTab()
+        initViewPager()
 
     }
 
@@ -65,6 +83,75 @@ class MainActivity : BaseActivity<MainPresenter>(),MainView {
 
     }
 
+    /*init view pager*/
+    private fun initTab() {
+        val tabs = arrayOf(content_layout.tabLayout.newTab().setText("Prev Match"), content_layout.tabLayout.newTab().setText("Next Match"))
 
-}
+        for (tab in tabs) {
+            val layout = LinearLayout(this)
+            layout.orientation = LinearLayout.VERTICAL
+            layout.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+            layout.weightSum = 1f
+            val newTab = ATextView(this)
+            newTab.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+            newTab.gravity = Gravity.CENTER
+            newTab.maxLines = 1
+            newTab.text = tab.text
+
+            newTab.setTextColor(ContextCompat.getColor(this, R.color.colorWhite))
+
+            layout.addView(newTab)
+
+            tab.customView = layout
+            content_layout.tabLayout.addTab(tab)
+        }
+
+        content_layout.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                fragment = fragments.get(tab.position)
+                content_layout.viewPager.setCurrentItem(tab.position)
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {
+
+            }
+        })
+    }
+
+    private fun initViewPager() {
+        fragments.add(Fragment())
+        fragments.add(Fragment())
+
+        fragment = fragments.get(0)
+        this.tabAdapter = LfPagerAdapter(supportFragmentManager,fragments)
+
+        content_layout.viewPager.setAdapter(tabAdapter)
+        content_layout.viewPager.setPagingEnabled(false)
+        content_layout.viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(content_layout.tabLayout))
+    }
+
+    class LfPagerAdapter (fm: FragmentManager, private val fragments:MutableList<Fragment>): FragmentStatePagerAdapter(fm)
+    {
+
+        private val NUM_ITEMS = 2
+
+
+
+        override fun getCount(): Int {
+            return NUM_ITEMS
+        }
+
+        override fun getItem(position: Int): Fragment {
+            when (position) {
+                1 -> return fragments[1]
+                else -> return fragments[0]
+            }
+        }
+
+
+}}
 
