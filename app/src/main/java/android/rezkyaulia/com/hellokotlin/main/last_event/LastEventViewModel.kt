6cfbe28a0745700269fa9c93db9122a01618c8f2
rezky aulia pratama <rezkyauliapratama@gmail.com>
2018-08-20 22:45:18 +0700

    - add fragment last and next event

---
+++ app/src/main/java/android/rezkyaulia/com/hellokotlin/main/last_event/LastEventViewModel.kt
@@ -0,0 +1,10 @@
+package android.rezkyaulia.com.hellokotlin.main.last_event
+
+import android.rezkyaulia.com.hellokotlin.base.BaseViewModel
+
+/**
+ * Created by Rezky Aulia Pratama on 20/8/18.
+ */
+class LastEventViewModel : BaseViewModel(){
+
+}
